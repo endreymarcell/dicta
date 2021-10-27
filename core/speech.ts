1053d54b0a7fcddb67fef82c1ee1b421267a7d40
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-27 10:25:54 +0200

    remove some anys

--- core/speech.ts
+++ core/speech.ts
@@ -5,7 +5,7 @@ import speech from '@google-cloud/speech';
 import { spawn } from 'child_process';
 import ws from 'ws';
 
-let vscode: WebSocket;
+let vscode: ws;  // Thanks a lot, ws
 
 const config = {
   encoding: 'LINEAR16' as const,
@@ -29,7 +29,7 @@ function broadcast(message: string) {
 
 function main() {
   const server = new ws.Server({ port: 7071 })
-  server.on('connection', (newClient: any) => {
+  server.on('connection', newClient => {
     console.log('VS Code has connected to the WS server');
     vscode = newClient
   })
@@ -39,7 +39,7 @@ function main() {
   const recognizeStream = client
     .streamingRecognize(request)
     .on('error', console.error)
-    .on('data', (data: any) => {
+    .on('data', data => {
       const result = data.results[0]?.alternatives[0]
       if (result) {
         console.log(result.transcript);
