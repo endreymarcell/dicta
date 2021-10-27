c4c91e13414458bdbacfec5dff0d6b36165c0de3
Marcell Endrey <marcell.endrey@prezi.com>
2021-10-27 14:34:34 +0200

    parsing starts to get working

--- core/lib/parser.test.ts
+++ core/lib/parser.test.ts
@@ -1,5 +1,41 @@
+import { parse, replaceSymbols, turnIntoVimCommand } from "./parser"
+
 describe('parser', () => {
-    test('tests are running', () => {
-        expect(true).toBe(false);
+    describe('replaceSymbols', () => {
+        test('no symbols', () => {
+            expect(replaceSymbols('foo bar baz')).toBe('foobarbaz');
+        })
+
+        test('single-word symbol', () => {
+            expect(replaceSymbols('foo space equals space one semicolon')).toBe('foo = 1;')
+        })
+
+        test('multiple-word symbols', () => {
+            expect(replaceSymbols('foo opening parenthesis single quote bar single quote closing parenthesis semicolon')).toBe("foo('bar');");
+        })
+    });
+
+    describe('turnIntoVimCommand', () => {
+        test('motion', () => {
+            expect(turnIntoVimCommand('down')).toBe('j');
+        })
+
+        test('count + motion', () => {
+            expect(turnIntoVimCommand('three times up')).toBe('3k')
+        })
+
+        test('multi-word paramless command', () => {
+            expect(turnIntoVimCommand('paste below')).toBe('p');
+        })
+
+        test('command with params', () => {
+            expect(turnIntoVimCommand('delete inside (')).toBe('di(');
+        })
+    })
+
+    describe('full parsing', () => {
+        test('full parsing', () => {
+            expect(parse('delete inside curly braces')).toBe('di}');
+        })
     })
 })
--- core/lib/parser.ts
+++ core/lib/parser.ts
@@ -1,3 +1,141 @@
+const numbers = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6'
+}
+
+const symbols = new Map<string, string>(Object.entries({
+    ...numbers,
+    'space': ' ',
+    'single quote': "'",
+    'single quotes': "'",
+    'double quote': '"',
+    'double quotes': '"',
+    'parenthesis': '(',
+    'opening parenthesis': '(',
+    'closing parenthesis': ')',
+    'parentheses': ')',
+    'curly braces': '}',
+    'opening curly': '{',
+    'closing curly': '}',
+    'brackets': ']',
+    'opening bracket': '[',
+    'closing bracket': ']',
+    'period': '.',
+    'dot': '.',
+    'comma': ',',
+    'question mark': '?',
+    'dash': '-',
+    'underscore': '_',
+    'slash': '/',
+    'semicolon': ';',
+    'equals': '=',
+    'double equals': '==',
+    'triple equals': '===',
+}));
+
+type Keyword = string | { char: string, doesExpectInput?: boolean };
+const keywords = new Map<string, Keyword>(Object.entries({
+    // Counts
+    ...numbers,
+    twice: '2',
+    times: '',
+    lines: '',
+    characters: '',
+
+    // Motions
+    go: '',
+    up: 'k',
+    down: 'j',
+    left: 'l',
+    right: 'r',
+    home: '^',
+    'to line start': '^',
+    'to the start of the line': '^',
+    end: '$',
+    'to line end': '$',
+    'to the end of the line': '$',
+    top: 'gg',
+    'to the top': 'gg',
+    'to the top of the document': 'gg',
+    bottom: 'G',
+    'to the bottom': 'G',
+    'to the bottom of the document': 'G',
+
+    // Text objects
+    inside: 'i',
+    in: 'i',
+    around: 'a',
+    surrounding: 's',
+    word: 'w',
+
+    insert: { char: 'i', doesExpectInput: true },
+    append: { char: 'a', doesExpectInput: true },
+    delete: 'd',
+    'paste above': 'P',
+    'paste below': 'p',
+    yank: 'y',
+    change: { char: 'c', doesExpectInput: true },
+    surround: { char: 'ys', doesExpectInput: true },
+}))
+
+export function replaceSymbols(input: string): string {
+    if (input === '') {
+        return '';
+    }
+
+    let originalSymbol: string = '';
+    for (const original of symbols.keys()) {
+        if (input.startsWith(original)) {
+            originalSymbol = original;
+            break;
+        }
+    }
+    if (originalSymbol !== '') {
+        return symbols.get(originalSymbol) + replaceSymbols(input.slice(originalSymbol.length));
+    } else {
+        if (input.includes(' ')) {
+            return input.slice(0, input.indexOf(' ')) + replaceSymbols(input.slice(input.indexOf(' ') + 1));
+        } else {
+            return input;
+        }
+    }
+}
+
+export function turnIntoVimCommand(input: string): string {
+    if (input === '') {
+        return '';
+    }
+
+    let originalKeyword: string = '';
+    for (const keyword of keywords.keys()) {
+        if (input.startsWith(keyword)) {
+            originalKeyword = keyword;
+            break;
+        }
+    }
+
+    if (originalKeyword !== '') {
+        const command = keywords.get(originalKeyword);
+        if (typeof command !== 'string' && command?.doesExpectInput) {
+            return command.char + input.slice(originalKeyword.length)
+        } else {
+            return command + turnIntoVimCommand(input.slice(originalKeyword.length));
+        }
+    } else {
+        if (input.includes(' ')) {
+            return input.slice(0, input.indexOf(' ')) + turnIntoVimCommand(input.slice(input.indexOf(' ') + 1));
+        } else {
+            return input;
+        }
+    }
+}
+
 export function parse(input: string) {
-    return input;
+    const inputWithSymbols = replaceSymbols(input);
+    const asVimCommand = turnIntoVimCommand(inputWithSymbols);
+    return asVimCommand;
 }
